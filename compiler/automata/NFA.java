3374eb4b85677909f9f189f09f751844074c8a8f
Arrij-Fawad <114739438+arrij46@users.noreply.github.com>
2025-02-19 22:43:00 +0500

    Update NFA.java

--- compiler/automata/NFA.java
+++ compiler/automata/NFA.java
@@ -1,1 +1,63 @@
 //replace later
+package compiler.automata;
+
+import java.util.*;
+
+public class NFA {
+    private State startState;
+    private Set<State> states;
+    private Set<State> acceptStates;
+    
+    public NFA() {
+        this.states = new HashSet<>();
+        this.acceptStates = new HashSet<>();
+    }
+    
+    public void setStartState(State state) {
+        this.startState = state;
+        states.add(state);
+    }
+    
+    public void addState(State state) {
+        states.add(state);
+        if (state.isAccepting()) {
+            acceptStates.add(state);
+        }
+    }
+    
+    public Set<State> getStates() {
+        return states;
+    }
+    
+    public State getStartState() {
+        return startState;
+    }
+    
+    public Set<State> getAcceptStates() {
+        return acceptStates;
+    }
+    
+    public Set<State> getNextStates(State currentState, char input) {
+        Set<State> nextStates = new HashSet<>();
+        State nextState = currentState.getNextState(input);
+        if (nextState != null) {
+            nextStates.add(nextState);
+        }
+        return nextStates;
+    }
+    public void printNFA()
+    {
+        System.out.println("Printing NFA: " + (startState != null ? startState : "None"));
+
+        System.out.println("States:");
+        for (State state : states) {
+            System.out.println("  " + state);
+        }
+
+        System.out.println("Accept States:");
+        for (State state : acceptStates) {
+            System.out.println("  " + state);
+        }
+    }
+
+}

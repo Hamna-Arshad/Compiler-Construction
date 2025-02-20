05ef4087cb2510bf676b03a23e9906bad11745ba
Arrij-Fawad <114739438+arrij46@users.noreply.github.com>
2025-02-20 18:46:01 +0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,28 @@
-
-
-
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        // Example source code
-        String sourceCode = """
-            dec  equal 10
-           
-            """;
+
+        StringBuilder SrcCode = new StringBuilder();
+        // Read File
+        try {
+            File myObj = new File("./SrcCode.txt");
+            Scanner myReader = new Scanner(myObj);
+            while (myReader.hasNextLine()) {
+                SrcCode.append(myReader.nextLine()).append("\n\r");
+                //System.out.println(SrcCode);
+            }
+            myReader.close();
+        } catch (FileNotFoundException e)
+        {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
 
         // Create lexical analyzer
-        lexer.LexicalAnalyzer analyzer = new lexer.LexicalAnalyzer(sourceCode);
+        lexer.LexicalAnalyzer analyzer = new lexer.LexicalAnalyzer(SrcCode);
         
         // Print NFA and DFA transitions
         System.out.println("Lexical Analysis Results:");

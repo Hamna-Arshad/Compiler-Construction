1cfc6df29f99ed86c976f62f5c55296145b6c034
Arrij-Fawad <114739438+arrij46@users.noreply.github.com>
2025-02-20 18:48:16 +0500

    Update State.java

--- src/lexer/model/State.java
+++ src/lexer/model/State.java
@@ -1,1 +1,61 @@
+package lexer.model;
 
+import java.util.*;
+
+public class State {
+    private int id;
+    private boolean isAccepting;
+    private ArrayList<Transition> transitions;
+
+    public State(int id) {
+        this.id = id;
+        this.isAccepting = false;
+        this.transitions = new ArrayList<>();
+    }
+
+    public void addTransition(char symbol, State target) {
+
+        transitions.add(new Transition(symbol, target));
+    }
+
+    public ArrayList<State> getTransitions(char symbol) {
+        ArrayList<State> targets = new ArrayList<>();
+        for (Transition t : transitions) {
+            if (t.symbol == symbol) {
+                targets.add(t.target);
+            }
+        }
+        return targets;
+    }
+
+    public ArrayList<Transition> getAllTransitions() {
+        return transitions;
+    }
+
+    public void setAccepting(boolean accepting) {
+        isAccepting = accepting;
+    }
+
+    public boolean isAccepting() {
+        return isAccepting;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    @Override
+    public String toString() {
+        return "q" + id + (isAccepting ? "(accepting)" : "");
+    }
+
+    private static class Transition {
+        char symbol;
+        State target;
+
+        Transition(char symbol, State target) {
+            this.symbol = symbol;
+            this.target = target;
+        }
+    }
+}
